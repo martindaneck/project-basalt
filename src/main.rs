ee84701d505cab48c4465188d73501e874692300
martinfallow <m.danek08@seznam.cz>
2025-12-15 21:38:11 +0100

    add empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
+    let (mut window, events) = glfw
+        .create_window(800, 600, "Hello Triangle", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    gl::load_with(|s| {
+        window.get_proc_address(s)
+            .map(|f| f as *const _)
+            .unwrap_or(std::ptr::null())
+    });
+
+    unsafe {
+        gl::Viewport(0, 0, 800, 600);
+    }
+
+    while !window.should_close() {
+        glfw.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
+            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                window.set_should_close(true);
+            }
+        }
+
+        unsafe {
+            gl::ClearColor(0.1, 0.1, 0.1, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.swap_buffers();
+    }
 }
