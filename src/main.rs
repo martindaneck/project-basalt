4a8af57f02e64f1485c9e873e01163032ff4cb8b
martinfallow <m.danek08@seznam.cz>
2025-12-17 14:43:08 +0100

    add Hello Triangle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use glfw::{Action, Context, Key};
+use std::ffi::CString;
+use std::ptr;
 
 fn main() {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
@@ -25,6 +27,78 @@ fn main() {
         gl::Viewport(0, 0, 800, 600);
     }
 
+    let vertices: [f32; 9] = [
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0,  0.5, 0.0,
+    ];
+
+    let mut vao: u32 = 0;
+    let mut vbo: u32 = 0;
+
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<f32>()) as isize,
+            vertices.as_ptr() as *const _,
+            gl::STATIC_DRAW,
+        );
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (3 * std::mem::size_of::<f32>()) as i32,
+            ptr::null(),
+        );
+        gl::EnableVertexAttribArray(0);
+    }
+
+    let vertex_src = CString::new(
+        "#version 460 core
+        layout (location = 0) in vec3 aPos;
+        void main() {
+            gl_Position = vec4(aPos, 1.0);
+        }"
+    ).unwrap();
+
+    let fragment_src = CString::new(
+        "#version 460 core
+        out vec4 FragColor;
+        void main() {
+            FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+        }"
+    ).unwrap();
+
+    let program: u32;
+
+    unsafe {
+        let vs = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(vs, 1, &vertex_src.as_ptr(), ptr::null());
+        gl::CompileShader(vs);
+
+        let fs = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(fs, 1, &fragment_src.as_ptr(), ptr::null());
+        gl::CompileShader(fs);
+
+        program = gl::CreateProgram();
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::LinkProgram(program);
+
+        gl::DeleteShader(vs);
+        gl::DeleteShader(fs);
+    }
+
+
+
     while !window.should_close() {
         glfw.poll_events();
 
@@ -37,7 +111,12 @@ fn main() {
         unsafe {
             gl::ClearColor(0.1, 0.1, 0.1, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::UseProgram(program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
+
 
         window.swap_buffers();
     }
