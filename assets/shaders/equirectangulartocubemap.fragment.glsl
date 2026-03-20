2cf61c29adecd7d5a83aa13bdf27b43754d940a7
martindaneck <m.danek08@seznam.cz>
2026-03-20 12:34:57 +0100

    small cleanup

--- assets/shaders/equirectangulartocubemap.fragment.glsl
+++ assets/shaders/equirectangulartocubemap.fragment.glsl
@@ -19,26 +19,4 @@ void main()
     vec2 uv = sampleSphericalMap(normalize(localPos));
     vec3 color = texture(equirectangularMap, uv).rgb;
     FragColor = vec4(color, 1.0);
-    //FragColor = vec4(normalize(localPos) * 0.5 + 0.5, 1.0); 
-    /*
-    vec3 d = vec3(debug);
-    if (d == 0.0){
-        d = vec3(1.0, 0.0, 0.0);
-    }
-    else if (d == 1.0){
-        d = vec3(0.0, 1.0, 0.0);
-    }
-    else if (d == 2.0){
-        d = vec3(0.0, 0.0, 1.0);
-    }
-    else if (d == 3.0){
-        d = vec3(1.0, 1.0, 0.0);
-    }
-    else if (d == 4.0){
-        d = vec3(1.0, 0.0, 1.0);
-    }
-    else if (d == 5.0){
-        d = vec3(0.0, 1.0, 1.0);
-    }
-    FragColor = vec4(d, 1.0); */
 }
